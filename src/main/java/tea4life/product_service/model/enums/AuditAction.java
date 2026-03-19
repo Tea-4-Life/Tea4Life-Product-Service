d652eec5b56d06582813a082c8d38d0e7a26248c
phatdang <phatdang19052004@gmail.com>
2026-03-19 16:18:12 +0700

    feat(model.enums): Implement product deletion audit logging and add audit service

---
+++ src/main/java/tea4life/product_service/model/enums/AuditAction.java
@@ -0,0 +1,12 @@
+package tea4life.product_service.model.enums;
+
+/**
+ * @author : user664dntp
+ * @mailto : phatdang19052004@gmail.com
+ * @created : 19/03/2026, Thursday
+ **/
+public enum AuditAction {
+    CREATE,
+    UPDATE,
+    DELETE
+}
