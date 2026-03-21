2e40ca1f0baa90cff3c68577b391be1c644ef576
phatdang <phatdang19052004@gmail.com>
2026-03-21 18:26:08 +0700

    refactor(service.impl): Rename ProductDeletedAuditEvent to ProductAuditEvent and update fields

--- src/main/java/tea4life/product_service/dto/event/ProductAuditEvent.java
+++ src/main/java/tea4life/product_service/dto/event/ProductAuditEvent.java
@@ -1,15 +1,17 @@
 package tea4life.product_service.dto.event;
 
+import tea4life.product_service.model.enums.AuditAction;
+
 /**
  * @author : user664dntp
  * @mailto : phatdang19052004@gmail.com
  * @created : 19/03/2026, Thursday
  **/
-public record ProductDeletedAuditEvent(
+public record ProductAuditEvent(
         Long productId,
         String productName,
-        String action,
-        Long performedBy,
+        AuditAction action,
+        String performedBy,
         long timestamp,
         String message
 ) {}
